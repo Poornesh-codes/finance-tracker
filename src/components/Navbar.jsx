8866024c8e99b87312a42d3547a767b2cfa48f7e
Poornesh-codes <poorneshk9@gmail.com>
2026-08-15 13:27:12 +0530

    navbar styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,32 @@
 import { Link } from 'react-router-dom'
+import './Navbar.css'
 
 function Navbar() {
   return (
-    <nav style={{ display: 'flex', gap: '1rem', padding: '1rem' }}>
-      <Link to="/">Dashboard</Link>
-      <Link to="/transactions">Transactions</Link>
-      <Link to="/add">Add Transaction</Link>
-      <Link to="/insights">Insights</Link>
+    <nav className="navbar">
+
+      <Link to="/" className="navbar-brand">
+        Finance Tracker
+      </Link>
+
+      <Link to="/" className="navbar-link">
+        Dashboard
+      </Link>
+
+      <Link to="/transactions" className="navbar-link">
+        Transactions
+      </Link>
+
+      <Link to="/insights" className="navbar-link">
+        Insights
+      </Link>
+
+      <Link to="/add" className="navbar-link navbar-add">
+        + Add Transaction
+      </Link>
+
     </nav>
   )
 }
+
 export default Navbar
